8403e381d0cbe645a7646e58cf5541149a72d0bc
Emmanuel Bett <emmanuelkbett3@gmail.com>
2023-09-24 21:24:48 +0300

    crop recom fixing

--- disease_detection/crop_prediction.py
+++ disease_detection/crop_prediction.py
@@ -40,27 +40,32 @@ def weather_fetch(msg_received):
     :params: city_name
     :return: temperature, humidity
     """
-    city_name = msg_received['city']
-    key = weather_config()
-    api_key = key['weather_api_key']
-    api_key = str(api_key)
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    try:
+            
+        city_name = msg_received['city']
+        key = weather_config()
+        api_key = key['weather_api_key']
+        api_key = str(api_key)
+        base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-    # msg_received['url'] = complete_url
-    # response = msg_received['url']
-    response = requests.get(complete_url)
-    x = response.json()
+        complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+        response = requests.get(complete_url)
+        x = response.json()
+        
+        
+        # Check if the 'main' key exists in the dictionary.
+        if "main" in x.keys():
+            # Get the temperature and humidity from the dictionary.
+            temperature = x["main"]["temp"]
+            humidity = x["main"]["humidity"]
 
-    if x["cod"] != "404":
-        y = x["main"]
-        print(y)
-        temperature = round((y["temp"] - 273.15), 2)
-        humidity = y["humidity"]
-        return temperature, humidity
-    else:
-        return None
-
+            return str(temperature), str(humidity)
+                  
+        else:
+            # The 'main' key does not exist, so raise an error.
+            raise KeyError("The 'main' key does not exist in the weather data response.")
+    except Exception as  e:
+        return {"error": str(e)}
 
 crop_recommendation_model_path = 'Trained_Model/RandomForest.pkl'
 crop_recommendation_model = pickle.load(
